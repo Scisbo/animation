432c13ed0a1fdb955faaf5e619768e841721a8e0
Volyarosh94 <vol.yarosh.94@gmail.com>
2024-05-13 11:17:20 +0300

    add responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import './App.css';
 
@@ -13,23 +13,50 @@ function App() {
 
 
 const Circle = ({ bgColor, left = 'none', top = 'none', width, height, right = 'none', bottom = 'none', borderWidth = '50px' }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  let customWidth = windowWidth > 768 ? width : `${parseInt(width) / 1.2}px`;
+  let customHeight = windowWidth > 768 ? height : `${parseInt(height) / 1.2}px`;
+  const customLeft = windowWidth > 768 ? left : `${parseInt(left) - 70}px`;
+  const customBorder = windowWidth > 420 ? borderWidth : `${0}px`;
+  let customDisplay = 'block';
+
+
+  if (customLeft === 'NaNpx') {
+    customDisplay = 'none';
+  }
+
   return (
     <motion.div
       initial={{ opacity: 0.5, }}
       whileHover={{ scale: 1.2, opacity: 1 }}
+      whileTap={{ scale: 1.2, opacity: 1 }}
       style={{
         borderRadius: "50%",
-        width: width,
-        height: height,
+        width: customWidth,
+        height: customHeight,
         backgroundColor: bgColor,
-        borderWidth: borderWidth,
+        borderWidth: customBorder,
         borderColor: 'white',
         borderStyle: 'solid',
         position: "absolute",
-        left: left,
+        left: customLeft,
         top: top,
         right: right,
-        bottom: bottom
+        bottom: bottom,
+        display: customDisplay
       }}
     ></motion.div>
   );
@@ -37,18 +64,18 @@ const Circle = ({ bgColor, left = 'none', top = 'none', width, height, right = '
 
 const MyComponent = () => {
   return (
-    <div style={{ position: "relative", width: '648px', height: '100vh' }}>
+    <div style={{ position: "relative", maxWidth: '648px', height: '100vh' }}>
       <Circle bgColor="#40b26b" left="140px" top="100px" width="80px" height="80px" />
-      <Circle bgColor="#F9C518" right="0" top="50px" width="200px" height="200px" />
+      <Circle bgColor="#F9C518" left="calc(100% - 200px)" top="50px" width="200px" height="200px" />
       <Circle bgColor="#F8922B" left="150px" top="280px" width="100px" height="100px" />
-      <Circle bgColor="black" right="90px" top="320px" width="50px" height="50px" borderWidth="20px" />
-      <Circle bgColor="grey" right="0px" top="350px" width="30px" height="30px" borderWidth="30px" />
+      <Circle bgColor="black" left="calc(100% - 140px)" top="320px" width="50px" height="50px" borderWidth="20px" />
+      <Circle bgColor="grey" left="calc(100% - 30px)" top="350px" width="30px" height="30px" borderWidth="30px" />
       <Circle bgColor="#CD5CE7" left="90px" top="450px" width="80px" height="80px" borderWidth="30px" />
       <Circle bgColor="#3AB8D1" left="250px" top="420px" width="200px" height="200px" borderWidth="30px" />
-      <Circle bgColor="#5D7CF9" right="0px" top="500px" width="100px" height="100px" borderWidth="20px" />
-      <Circle bgColor="#F26497" right="100px" top="650px" width="70px" height="70px" borderWidth="30px" />
+      <Circle bgColor="#5D7CF9" left="calc(100% - 100px)" top="500px" width="100px" height="100px" borderWidth="20px" />
+      <Circle bgColor="#F26497" left="calc(100% - 170px)" top="650px" width="70px" height="70px" borderWidth="30px" />
       <Circle bgColor="#94D82D" left="150px" top="700px" width="200px" height="200px" borderWidth="20px" />
-      <Circle bgColor="#4AA9FD" right="0px" top="750px" width="50px" height="50px" borderWidth="20px" />
+      <Circle bgColor="#4AA9FD" left="calc(100% - 50px)" top="750px" width="50px" height="50px" borderWidth="20px" />
       {/* Add more <Circle /> components as needed */}
     </div>
   );
